159d2d57d26a3346488e337772c643418f1ae0aa
Yunare <yu.na.re.me@gmail.com>
2023-06-28 12:24:10 +0300

    task_3_1

--- js/task_3.js
+++ js/task_3.js
@@ -1,12 +1,8 @@
 // Написати функцію яка приймає строку-слово в нижнему регестрі і повертаж слово де перша буква в верхнему реестрі 
 // upperFirstWord(‘cat’) => ‘Cat’
 
-function upperFirstWord(str) {
-    console.log(str);
-    let newStr = str[0].toUpperCase() + str.slice(1);
-    console.log(newStr)
-    return newStr;
-};
 
-upperFirstWord('стрічка');
 
+const upperFirstWord = (str) => str[0].toUpperCase() + str.slice(1);
+
+console.log(upperFirstWord('стрічка'));
